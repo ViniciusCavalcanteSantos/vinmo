6d778708fca62f24bffd06c8084d578a43bbf11f
vinicius <viniciuscsantosoficial@gmail.com>
2025-09-23 11:30:16 -0300

    lang: traduz o antd conforme o i18next

--- client/src/app/[lng]/providers.tsx
+++ client/src/app/[lng]/providers.tsx
@@ -3,15 +3,25 @@
 import {AntdRegistry} from "@ant-design/nextjs-registry";
 import {PropsWithChildren} from "react";
 import {ConfigProvider} from "antd";
-import ptBR from 'antd/locale/pt_BR';
 import {themeAntd} from "@/theme";
 import {NotificationProvider} from "@/contexts/NotificationContext";
+import en from 'antd/locale/en_US';
+import ptBR from 'antd/locale/pt_BR';
+import i18next from "@/i18n/i18next";
+import {Locale} from "antd/es/locale";
+
 
 export default function Providers({children}: PropsWithChildren) {
+  const lang = i18next.language
+  const langMap: Record<string, Locale>  = {
+    'en': en,
+    'pt-BR': ptBR
+  }
+
   return (
     <AntdRegistry>
       <ConfigProvider
-        locale={ptBR}
+        locale={langMap[lang] ?? en}
         theme={{
           token: themeAntd
         }}
