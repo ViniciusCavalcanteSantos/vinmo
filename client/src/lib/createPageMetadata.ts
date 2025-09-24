228881d88e1018a01db368266bc5521e9c80e5f8
vinicius <viniciuscsantosoficial@gmail.com>
2025-09-24 11:19:43 -0300

    feat: altera layout para poder alterar o SEO com base na página
    agora o SEO pode ser alterado de forma dinâmica, mudando canonical, hreflang, title, description e muitas outras funcionalidades conforme a página

---
+++ client/src/lib/createPageMetadata.ts
@@ -0,0 +1,36 @@
+import type { Metadata, ResolvingMetadata } from "next";
+import {getT} from "@/i18n";
+
+type Options = {
+  path: string;
+  titleKey: string;
+  descriptionKey: string;
+};
+
+async function createPageMetadata(
+  { path, titleKey, descriptionKey }: Options,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const parentMetadata = await parent;
+  const appName = process.env.NEXT_PUBLIC_APP_NAME ?? "Vinmo";
+  const baseUrl = String(parentMetadata.alternates?.canonical?.url || "https://vinmo.org");
+
+  const { t } = await getT();
+  const title = t(titleKey);
+  const description = t(descriptionKey);
+
+  return {
+    title: `${title} | ${appName}`,
+    description,
+    alternates: {
+      canonical: `${baseUrl}/${path}`,
+      languages: {
+        en: `${baseUrl?.replace(/\/pt-BR$/, "/en")}/${path}`,
+        "pt-BR": `${baseUrl?.replace(/\/en$/, "/pt-BR")}/${path}`,
+        "x-default": `${baseUrl?.replace(/\/pt-BR$/, "/en")}/${path}`,
+      },
+    },
+  };
+}
+
+export default createPageMetadata;
