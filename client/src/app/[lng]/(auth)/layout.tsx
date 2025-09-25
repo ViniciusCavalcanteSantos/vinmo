c89ddf098a4c6fd298978e655466795e6a8ff392
vinicius <viniciuscsantosoficial@gmail.com>
2025-09-25 11:25:39 -0300

    feat: melhora o redirecionamento se o usuário estiver logado

--- client/src/app/[lng]/(auth)/layout.tsx
+++ client/src/app/[lng]/(auth)/layout.tsx
@@ -1,14 +1,22 @@
 "use client"
 
-import {PropsWithChildren, useEffect} from "react";
+import {PropsWithChildren, useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
+import Fallback from "@/components/Fallback";
 
 export default function Layout({children}: PropsWithChildren) {
   const router = useRouter();
+  const [checking, setChecking] = useState(true);
+
   useEffect(() => {
     const token = localStorage.getItem("token")
-    if(token) router.push("/home")
+    if(token) router.replace("/home")
+    else {
+      setChecking(false);
+    }
   }, [])
+
+  if(checking) return <Fallback />;
 
   return (
     <div className="h-full flex justify-center items-center">
