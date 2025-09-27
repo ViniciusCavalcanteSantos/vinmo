f6e0916ba7d417c9b0c3666963206c80f88439b4
vinicius <viniciuscsantosoficial@gmail.com>
2025-09-27 17:30:51 -0300

    feat: adiciona as tabelas relacionadas ao contrato

---
+++ server/database/migrations/2025_09_26_230815_create_contracts_graduation_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contracts_graduation_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('contract_id')->unique();
+            $table->foreign('contract_id')->references('id')->on('contracts')->onDelete('cascade');
+            $table->enum('type', ['university', 'school']);
+            $table->string('institution_name', 180);
+            $table->string('institution_acronym', 20)->nullable();
+            $table->string('class', 40);
+            $table->enum('shift', ['morning', 'afternoon', 'night', 'full_time']);
+            $table->year('conclusion_year');
+
+            $table->string('university_course', 120)->nullable();
+            $table->enum('school_grade_level', ['elementary_school', 'middle_school', 'high_school'])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contracts_graduation_details');
+    }
+};
