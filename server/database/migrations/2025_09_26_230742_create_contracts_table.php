b2972571316bdd06081bd370476b1dcc1cda4043
vinicius <viniciuscsantosoficial@gmail.com>
2025-09-29 16:31:29 -0300

    chore: deixa o código unico por usuário

--- server/database/migrations/2025_09_26_230742_create_contracts_table.php
+++ server/database/migrations/2025_09_26_230742_create_contracts_table.php
@@ -22,6 +22,8 @@ return new class extends Migration {
             $table->text('searchable')->nullable();
             $table->fullText('searchable');
             $table->timestamps();
+
+            $table->unique(['user_id', 'code']);
         });
     }
 
