16299e62a8ffb4c56898ba5b888f65c6f84f5527
vinicius <viniciuscsantosoficial@gmail.com>
2025-09-29 11:24:25 -0300

    feat: adiciona coluna searchable aos contratos

--- server/database/migrations/2025_09_26_230742_create_contracts_table.php
+++ server/database/migrations/2025_09_26_230742_create_contracts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -19,6 +18,9 @@ return new class extends Migration
             $table->foreign('category_id')->references('id')->on('contracts_categories');
             $table->string('code', 40);
             $table->string('title', 180);
+
+            $table->text('searchable')->nullable();
+            $table->fullText('searchable');
             $table->timestamps();
         });
     }
