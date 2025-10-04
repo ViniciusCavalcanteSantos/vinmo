8b4dad337f7d31212a0fddf2e8b651cd01ae5abe
vinicius <viniciuscsantosoficial@gmail.com>
2025-10-03 21:06:19 -0300

    fix: troca o FormRequest pelo ApiFormRequest das requests

--- server/app/Http/Requests/EventRequest.php
+++ server/app/Http/Requests/EventRequest.php
@@ -3,10 +3,9 @@
 namespace App\Http\Requests;
 
 use App\Models\Contract;
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class EventRequest extends FormRequest
+class EventRequest extends ApiFormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
