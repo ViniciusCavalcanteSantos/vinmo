b4b7fb3aec2993ca190136a298f4c9225dfd9683
vinicius <viniciuscsantosoficial@gmail.com>
2025-10-06 08:32:23 -0300

    feat: adiciona os ClientType

---
+++ client/src/types/ClientType.ts
@@ -0,0 +1,28 @@
+import {FullAddressType} from "@/types/AddressType";
+
+export const guardianTypes = [
+  'mother', 'father', 'grandmother', 'grandfather', 'uncle', 'aunt',
+  'sister', 'brother', 'godmother', 'godfather', 'other'
+] as const;
+
+export type GuardianType = typeof guardianTypes[number];
+
+type ClientType = {
+  id: number,
+  userId: number,
+  name: string,
+  pictureUrl: string,
+  code?: string,
+  birthdate?: string,
+  phone?: string,
+  guardian?: {
+    name: string,
+    type: GuardianType,
+    email?: string,
+    phone?: string,
+  },
+  address?: FullAddressType
+  createdAt: string
+}
+
+export default ClientType
