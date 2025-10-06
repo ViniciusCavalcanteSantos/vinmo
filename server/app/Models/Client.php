ebd915ab2355675bd8b314f04349d2021ff6088c
vinicius <viniciuscsantosoficial@gmail.com>
2025-10-06 08:35:41 -0300

    feat: adiciona a tabela de clientes

--- server/app/Models/Client.php
+++ server/app/Models/Client.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Client extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'code', 'name', 'birthdate', 'phone', 'picture_url',
+        'guardian_name', 'guardian_type', 'guardian_email', 'guardian_phone',
+    ];
+
+    protected $casts = [
+        'birthdate' => 'date',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Um Cliente pode ter um endereço (ocal onde mora).
+     */
+    public function address(): MorphOne
+    {
+        return $this->morphOne(Address::class, 'addressable');
+    }
 }
--- server/database/migrations/2025_10_03_223348_create_clients_table.php
+++ server/database/migrations/2025_10_03_223348_create_clients_table.php
@@ -12,6 +12,24 @@ return new class extends Migration {
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('name', 60);
+            $table->string('picture_url');
+
+            $table->string('code', 20)->nullable();
+            $table->date('birthdate')->nullable();
+            $table->string('phone', 20)->nullable();
+
+            $table->string('guardian_name', 60)->nullable();
+            $table->enum('guardian_type', [
+                'mother', 'father', 'grandmother', 'grandfather', 'uncle', 'aunt',
+                'sister', 'brother', 'godmother', 'godfather', 'other'
+            ])->nullable();
+            $table->string('guardian_email', 60)->nullable();
+            $table->string('guardian_phone', 20)->nullable();
+
             $table->timestamps();
         });
     }
