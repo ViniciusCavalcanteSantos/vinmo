50bab162687ae4878f66f216b71d5bab09f13603
vinicius <viniciuscsantosoficial@gmail.com>
2025-10-08 13:04:31 -0300

    feat: cria o UserContext

--- client/src/app/[lng]/(user)/providers.tsx
+++ client/src/app/[lng]/(user)/providers.tsx
@@ -1,15 +1,18 @@
 import {ContractsProvider} from "@/contexts/ContractsContext";
 import {EventsProvider} from "@/contexts/EventsContext";
 import {ClientsProvider} from "@/contexts/ClientsContext";
+import {UserProvider} from "@/contexts/UserContext";
 
 export default function Providers({children}: { children: React.ReactNode }) {
   return (
-    <ContractsProvider>
-      <EventsProvider>
-        <ClientsProvider>
-          {children}
-        </ClientsProvider>
-      </EventsProvider>
-    </ContractsProvider>
+    <UserProvider>
+      <ContractsProvider>
+        <EventsProvider>
+          <ClientsProvider>
+            {children}
+          </ClientsProvider>
+        </EventsProvider>
+      </ContractsProvider>
+    </UserProvider>
   );
 }
---
+++ client/src/contexts/UserContext.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import {createContext, useCallback, useContext, useEffect, useMemo, useState} from 'react';
+import User from "@/types/User";
+import getDateFormatByCountry from "@/lib/getDateFormatByCountry";
+
+interface UserContextType {
+  user: User | null;
+  setUser: (user: User) => void;
+  logout: () => void;
+  defaultDateFormat: string;
+}
+
+const UserContext = createContext<UserContextType | null>(null);
+
+export const UserProvider = ({children}: { children: React.ReactNode }) => {
+  const [user, setUserState] = useState<User | null>(null);
+  const [defaultDateFormat, setDefaultDateFormat] = useState<string>('MM/DD/YYYY');
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (!storedUser) return;
+
+    const userData = JSON.parse(storedUser) as User;
+    setUser(userData)
+    setDefaultDateFormat(getDateFormatByCountry(userData.address.country ?? null));
+  }, []);
+
+  const setUser = useCallback((userToSet: User) => {
+    localStorage.setItem("user", JSON.stringify(userToSet));
+    setUserState(userToSet);
+  }, [setUserState]);
+
+  const logout = useCallback(() => {
+    localStorage.removeItem("user");
+    setUserState(null);
+  }, [setUserState]);
+
+
+  const value = useMemo(() => ({
+    user,
+    setUser,
+    logout,
+    defaultDateFormat
+  }), [user, setUser, logout, defaultDateFormat]);
+
+  return (
+    <UserContext.Provider value={value}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext)
+
+  if (context === null) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+
+  return context;
+}
