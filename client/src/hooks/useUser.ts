96aecbc70a4b71221325f4a2853dd8dfef374189
vinicius <viniciuscsantosoficial@gmail.com>
2025-11-06 10:14:36 -0300

    fix: remove unused hook "useUser"

--- client/src/hooks/useUser.ts
+++
@@ -1,6 +0,0 @@
-import {useLocalStorage} from "react-use";
-import User from "@/types/User";
-
-export default function useUser(): User {
-  return (useLocalStorage('user')[0]) as User ?? undefined;
-}
