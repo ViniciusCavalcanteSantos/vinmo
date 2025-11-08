085dde6a97dcfbd1c8a55558e0e4edc5dfcfe959
vinicius <viniciuscsantosoficial@gmail.com>
2025-11-07 23:17:09 -0300

    feat: adiciona a url do client ao cors por padrão

--- server/config/cors.php
+++ server/config/cors.php
@@ -21,7 +21,8 @@ return [
 
     'allowed_origins' => [
         'http://localhost:3000',
-        'http://127.0.0.1:3000'
+        'http://127.0.0.1:3000',
+        env('APP_URL_CLIENT')
     ],
 
     'allowed_origins_patterns' => [],
