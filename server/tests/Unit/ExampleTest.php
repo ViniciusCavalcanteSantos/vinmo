1797a328a4285d076fbdfcf68f01a3b22a8b50bb
vinicius <viniciuscsantosoficial@gmail.com>
2025-11-17 10:26:56 -0300

    rename; troca o nome de .env.dev.example para .env.docker.example

--- server/tests/Unit/ExampleTest.php
+++ server/tests/Unit/ExampleTest.php
@@ -1,13 +1,5 @@
 <?php
 
 test('that true is true', function () {
-
-    try {
-        $res = env('DB_CONNECTION');
-        dump($res);
-    } catch (\Exception $e) {
-
-        dump($e->getMessage());
-    }
     expect(true)->toBeTrue();
 });
