12c55ea650d2084e422d57971b1bce5bffa21d00
vinicius <viniciuscsantosoficial@gmail.com>
2025-11-29 15:42:31 -0300

    feat: cria componentes de icones do Google e Microsoft

---
+++ client/src/components/Icons/IconMicrosoft.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const IconMicrosoft = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    className="w-6 h-6"
+    viewBox="0 0 23 23"
+    {...props}
+  >
+    <rect x="1" y="1" width="10" height="10" fill="#F25022"/>
+    <rect x="12" y="1" width="10" height="10" fill="#7FBA00"/>
+    <rect x="1" y="12" width="10" height="10" fill="#00A4EF"/>
+    <rect x="12" y="12" width="10" height="10" fill="#FFB900"/>
+  </svg>
+);
+
+export default React.memo(IconMicrosoft);
