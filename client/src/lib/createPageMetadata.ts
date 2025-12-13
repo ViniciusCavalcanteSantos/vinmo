66cb6d88fb8229d5369401c215becc7a06876659
vinicius <viniciuscsantosoficial@gmail.com>
2025-12-13 18:10:19 -0300

    feat: melhora a função de SEO createPageMetadata

--- client/src/lib/createPageMetadata.ts
+++ client/src/lib/createPageMetadata.ts
@@ -5,31 +5,59 @@ type Options = {
   path: string;
   titleKey: string;
   descriptionKey: string;
+  robots?: Metadata["robots"];
+  image?: string;
+  isAppRoute?: boolean;
 };
 
 async function createPageMetadata(
-  {path, titleKey, descriptionKey}: Options,
+  {path, titleKey, descriptionKey, robots, image, isAppRoute = false}: Options,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
   const parentMetadata = await parent;
-  const appName = process.env.NEXT_PUBLIC_APP_NAME ?? "App";
-  const baseUrl = String(parentMetadata.alternates?.canonical?.url || "https://photon.org");
+  const appName = process.env.NEXT_PUBLIC_APP_NAME ?? "Photon";
+
+  let baseUrlString = String(parentMetadata.alternates?.canonical?.url || "https://photon.org");
+  baseUrlString = baseUrlString.replace(/\/$/, "").replace(/\/(pt-BR|en)$/, "");
 
   const {t} = await getT();
   const title = t(titleKey);
   const description = t(descriptionKey);
+  const defaultImage = "/og-default.png";
+
+  const alternates = isAppRoute
+    ? {
+      canonical: `${baseUrlString}/${path}`,
+    }
+    : {
+      canonical: `${baseUrlString}/${path}`,
+      languages: {
+        en: `${baseUrlString}/en/${path}`,
+        "pt-BR": `${baseUrlString}/pt-BR/${path}`,
+        "x-default": `${baseUrlString}/en/${path}`,
+      },
+    };
 
   return {
     title: `${title} | ${appName}`,
     description,
-    alternates: {
-      canonical: `${baseUrl}/${path}`,
-      languages: {
-        en: `${baseUrl?.replace(/\/pt-BR$/, "/en")}/${path}`,
-        "pt-BR": `${baseUrl?.replace(/\/en$/, "/pt-BR")}/${path}`,
-        "x-default": `${baseUrl?.replace(/\/pt-BR$/, "/en")}/${path}`,
-      },
+    robots: robots,
+    openGraph: {
+      title: `${title} | ${appName}`,
+      description,
+      url: `${baseUrlString}/${path}`,
+      siteName: appName,
+      images: [{url: image || defaultImage, width: 1200, height: 630}],
+      type: "website",
+      locale: "pt_BR",
     },
+    twitter: {
+      card: "summary_large_image",
+      title: `${title} | ${appName}`,
+      description,
+      images: [image || defaultImage],
+    },
+    alternates: alternates,
   };
 }
 
