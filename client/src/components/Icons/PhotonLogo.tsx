0003edecf7ada247cc2eeedc577f673f23edf94e
vinicius <viniciuscsantosoficial@gmail.com>
2025-12-21 08:56:53 -0300

    feat: adiciona componente PhotonLogo como SVG

---
+++ client/src/components/Icons/PhotonLogo.tsx
@@ -0,0 +1,93 @@
+import * as React from "react";
+
+export default function PhotonLogo(
+  {
+    className,
+    ...props
+  }: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="124.16767mm"
+      height="122.41286mm"
+      viewBox="0 0 124.16767 122.41286"
+      version="1.1"
+      id="svg1"
+      xmlSpace="preserve"
+      xmlns="http://www.w3.org/2000/svg"
+      className={`w-6 h-6 ${className}`}
+      {...props}
+    >
+      <defs id="defs1">
+        <linearGradient id="linearGradient12">
+          <stop style={{stopColor: '#78ebff', stopOpacity: 1}} offset="0" id="stop12"/>
+          <stop style={{stopColor: '#0458cf', stopOpacity: 1}} offset="1" id="stop13"/>
+        </linearGradient>
+        <linearGradient id="linearGradient10">
+          <stop style={{stopColor: '#78ebff', stopOpacity: 1}} offset="0" id="stop10"/>
+          <stop style={{stopColor: '#0458cf', stopOpacity: 1}} offset="1" id="stop11"/>
+        </linearGradient>
+        <linearGradient id="linearGradient8">
+          <stop style={{stopColor: '#78ebff', stopOpacity: 1}} offset="0" id="stop8"/>
+          <stop style={{stopColor: '#0458cf', stopOpacity: 1}} offset="1" id="stop9"/>
+        </linearGradient>
+        <linearGradient
+          id="linearGradient9"
+          x1="53.511475"
+          y1="33.818645"
+          x2="149.15927"
+          y2="91.289635"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(5.9868374,78.951418)"
+          xlinkHref="#linearGradient8"
+        />
+        <linearGradient
+          id="linearGradient11"
+          x1="38.374439"
+          y1="97.592789"
+          x2="88.392303"
+          y2="131.33026"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(5.9868374,78.951418)"
+          xlinkHref="#linearGradient10"
+        />
+        <linearGradient
+          id="linearGradient13"
+          x1="-95.498993"
+          y1="32.951782"
+          x2="-143.2269"
+          y2="65.144669"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(-5.9868374,78.951418)"
+          xlinkHref="#linearGradient12"
+        />
+      </defs>
+      <g id="layer1" transform="translate(-39.281052,-90.509751)">
+        <ellipse
+          style={{mixBlendMode: 'normal', fill: 'url(#linearGradient13)', strokeWidth: 0.276168}}
+          id="circle2-8"
+          cx="-125.34978"
+          cy="127.99964"
+          transform="scale(-1,1)"
+          rx="28.785139"
+          ry="28.644823"
+        />
+        <path
+          style={{fill: 'url(#linearGradient11)', stroke: 'none', strokeWidth: 0.264583, strokeOpacity: 1}}
+          d="m 39.281051,210.84903 c 0,0 13.50721,0.009 22.838257,-7.89562 9.331049,-7.9045 11.52934,-14.40583 11.52934,-14.40583 l 1.847501,-4.34981 1.262848,-2.61924 c 0,0 2.011204,-4.2095 6.781967,-6.52473 4.770765,-2.31522 11.412415,-0.1637 11.412415,-0.1637 0,0 3.60146,1.30962 4.18611,1.75396 0.58465,0.44433 0.537879,0.54372 0.537879,0.54372 0,0 -2.361999,0.1812 -3.975639,1.77151 -1.61364,1.59025 -3.34421,3.15712 -4.5369,5.77636 -1.19269,2.61924 -3.41437,6.40779 -4.39658,8.02143 -0.98222,1.61364 -7.683943,11.42776 -16.084464,15.26422 -8.40052,3.83646 -10.318749,3.83646 -18.190103,4.69635 -7.871355,0.8599 -11.740889,-1.22371 -13.212631,-1.86862 z"
+          id="path15"
+        />
+        <path
+          style={{
+            fill: 'url(#linearGradient9)',
+            fillOpacity: 1,
+            stroke: 'none',
+            strokeWidth: 0.264583,
+            strokeOpacity: 1
+          }}
+          d="m 51.52928,192.12256 c 0,0 3.782791,-6.8642 9.405186,-11.56055 5.622396,-4.69636 10.867872,-6.89108 15.207329,-7.8733 4.339455,-0.98221 8.829585,-2.52569 20.101677,0.23386 11.272088,2.75956 12.035198,3.79688 16.908138,4.25627 4.9273,0.46451 11.10839,1.19269 21.18779,-2.99342 10.0794,-4.1861 15.17757,-10.78098 15.17757,-10.78098 0,0 3.52867,-3.14176 8.25461,-11.96231 0,0 5.06274,-8.68359 5.61266,-20.00245 0.29109,-5.99146 -0.35008,-12.04785 -3.01612,-17.80082 -2.66601,-5.75298 -8.98026,-16.510572 -23.01191,-21.187792 -14.03165,-4.67722 -25.98555,0.56693 -25.98555,0.56693 0,0 -10.05416,3.70416 -17.660934,14.287502 -7.60677,10.58333 -6.283854,24.40781 -6.283854,24.40781 v 0 c 0,0 -0.06615,-10.78177 4.233334,-18.98386 4.299479,-8.20208 13.559894,-16.073432 20.372914,-18.190102 6.81302,-2.11667 14.41979,-3.57187 24.93698,-0.26458 10.51719,3.30729 17.16581,12.766142 17.16581,12.766142 0,0 7.44045,8.84453 6.77899,20.95872 -0.66146,12.1142 -3.76838,16.67826 -6.77899,21.6392 -3.0106,4.96093 -12.7563,11.89472 -19.95921,13.39143 -7.20291,1.49671 -12.81702,1.35908 -22.6066,-0.95602 -9.78958,-2.3151 -17.877386,-7.7436 -35.427305,-0.72778 -17.549918,7.01583 -23.349666,27.26819 -23.349666,27.26819 l -1.122532,3.08696 z"
+          id="path20"
+        />
+      </g>
+    </svg>
+  );
+}
